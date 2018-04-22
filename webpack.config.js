3ab35f36a08aaf1096f905e0d596f4750c7c275e
Danston <thedanevans@gmail.com>
2018-04-21 17:08:26 -0700

    Rearrange files into a src directory
    This will make it cleaner to generate a built index.js file file for
    github pages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
-    filename: 'bundle.js'
+    filename: 'index.js',
+    path: path.resolve(__dirname)
   },
   module: {
     rules: [
